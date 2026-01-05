c2f65d398ef5b369182b588de95e3030a6e6587d
Rishaha <rishurawatx77@gmail.com>
2026-01-05 09:24:24 +0530

    leetcode 121 soln

---
+++ Arrays/Easy/best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for(int i = 0; i < prices.size(); i++) {
+            maxProfit = max(maxProfit, prices[i]-minPrice);
+            minPrice = min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+};
